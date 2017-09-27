37362e8abc12d57b4f71c52db0b9790086aec6f3
Mueed Chaudhry <mueed.chaudhry2@gmail.com>
2017-09-27 16:51:46 -0400

    finishing chapter 3

--- src/pledge.js
+++ src/pledge.js
@@ -9,10 +9,11 @@ function $Promise(executor) {
   }
   this._state = 'pending';
   this._value = undefined;
-  
-  const resolve = this._internalResolve.bind(this);
-  const reject = this._internalReject.bind(this)
 
+  const resolve = this._internalResolve.bind(this);
+  const reject = this._internalReject.bind(this);
+
+  this._handlerGroups = [];
   executor(resolve, reject);
 }
 
@@ -20,14 +21,37 @@ $Promise.prototype._internalResolve = function (data) {
   if (this._state === 'pending') {
     this._value = data;
     this._state = 'fulfilled';
+    this._callHandlers();
   }
 };
 $Promise.prototype._internalReject = function (reason) {
   if (this._state === 'pending') {
     this._value = reason;
     this._state = 'rejected';
+    this._callHandlers();
   }
 };
+
+$Promise.prototype.then = function (successCb, errorCb) {
+  if (typeof successCb !== 'function') successCb = undefined;
+  if (typeof errorCb !== 'function') errorCb = undefined;
+  this._handlerGroups.push({ successCb, errorCb });
+  if (this._state !== 'pending') this._callHandlers();
+}
+
+$Promise.prototype._callHandlers = function () {
+  this._handlerGroups.forEach((group) => {
+    if (group.successCb) {
+      group.successCb(this._value);
+      this._handlerGroups = [];
+    }
+    if (group.errorCb) {
+      group.errorCb(this._value);
+      this._handlerGroups = [];
+    }
+  })
+}
+
 
 /*-------------------------------------------------------
 The spec was designed to work with Test'Em, so we don't
